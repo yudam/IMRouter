d881d777e4421228d5093c904dcaba8dac10f319
yudam <2860605483@qq.com>
2019-08-30 21:07:07 +0800

    添加APT

--- imrouter_compiler/src/main/java/com/imrouter/compiler/IMRouterProcessor.java
+++ imrouter_compiler/src/main/java/com/imrouter/compiler/IMRouterProcessor.java
@@ -3,15 +3,18 @@ package com.imrouter.compiler;
 import com.imrouter.annotation.Router;
 
 import java.util.Collections;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 
 /**
  * User: maodayu
@@ -41,8 +44,23 @@ public class IMRouterProcessor extends AbstractProcessor {
         Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(Router.class);
         String packages = processingEnv.getOptions().get("IMROUTER");
         looger(packages);
-
         if (elements != null && elements.size() > 0) {
+            for (Element element : elements) {
+                if (element.getKind() == ElementKind.CLASS) {
+
+                    TypeElement typeElement = (TypeElement) element;
+
+                    //获取注解相关信息
+                    Router router = element.getAnnotation(Router.class);
+
+                    looger(router.path());
+
+                    looger(element.getSimpleName().toString());
+                    looger(typeElement.getQualifiedName().toString());
+
+                    TypeMirror typeMirror = element.asType();
+                }
+            }
 
             return true;
         }
